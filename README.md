7a115c76921de74ec885e214e368a2ec237a978c
Sharooq Salaudeen <coolsharooqs@gmail.com>
2024-03-01 15:31:35 +0000

    01March24 update 3

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Algorithms and Data Structures Solutions
+# Files in Folder
+
+- Leetcode 20 valid parentheses.py
+- Leetcode 26 removing duplicates from sorted array
+- Leetcode 27 removing elements from an integer array
+- Leetcode 88 merge sorted array
+- Leetcode 155 designing a minstack with constant time complexity
+- Leetcode 682 solving a baseball scoring puzzle
+- Leetcode 1929 solving the array concatenation problem
---
+++ readme-generator.py
@@ -0,0 +1,47 @@
+import os
+import re
+
+folder_path = os.path.dirname(os.path.abspath(__file__))
+
+# Function to get all file names in a folder
+def list_files(folder_path):
+    ignore_files = ["readme-generator.py", ".git", "README.md", ".DS_Store", ".gitignore" ]
+    files = os.listdir(folder_path)
+    # Filter out ignored files
+    files = [file for file in files if file not in ignore_files]
+    return files
+
+# Function to format file names
+def format_file_names(files):
+    formatted_files = []
+    for file in files:
+        # Remove file extension
+        file_name = os.path.splitext(file)[0]
+        # Replace '-' with space and capitalize first letter
+        formatted_name = file_name.replace("-", " ").capitalize()
+        formatted_files.append(formatted_name)
+    return formatted_files
+
+# Function to write file names to README.md
+def write_to_readme(files):
+    with open("README.md", "w") as readme_file:
+        readme_file.write("# Files in Folder\n\n")
+        for file in files:
+            readme_file.write(f"- {file}\n")
+
+# Function to sort formatted file names based on numerical count
+def sort_file_names(files):
+    sorted_files = sorted(files, key=lambda x: [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', x)])
+    return sorted_files
+
+# Get the list of files
+files = list_files(folder_path)
+
+# Format file names
+formatted_files = format_file_names(files)
+
+# Sort formatted file names word by word
+sorted_files = sort_file_names(formatted_files)
+
+# Write the sorted and formatted file names to README.md
+write_to_readme(sorted_files)
