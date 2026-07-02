303ebca46dd468e819749b135440938c613de968
Sharooq Salaudeen <coolsharooqs@gmail.com>
2026-07-02 10:18:28 +0100

    contains duplicate update

--- leetcode-contains-duplicate.py
+++ leetcode-contains-duplicate.py
@@ -3,4 +3,28 @@ from typing import List
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(0, len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) < len(nums)
