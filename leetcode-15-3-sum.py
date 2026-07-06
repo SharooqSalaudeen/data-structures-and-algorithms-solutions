d416da0d57bd6bdab41c15afb58f86349b05b5a8
Sharooq Salaudeen <coolsharooqs@gmail.com>
2026-07-06 11:49:08 +0100

    3 sum

---
+++ leetcode-15-3-sum.py
@@ -0,0 +1,43 @@
+from typing import List
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        tmp = [nums[i], nums[j], nums[k]]
+                        res.add(tuple(tmp))
+
+        return [list(i) for i in res ]
+    
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+            
+            if i > 0 and a == nums[i-1]:
+                continue
+            
+            l, r = i+1, len(nums)-1
+            while l<r:
+                threesum =  a + nums[l] + nums[r]
+                if threesum > 0:
+                    r -= 1
+                elif threesum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+        return res
+                
+
