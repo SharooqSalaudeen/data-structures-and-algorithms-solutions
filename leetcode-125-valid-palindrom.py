c5987047deca0a7fb9e879cab7f5dc5e8aaa557d
Sharooq Salaudeen <coolsharooqs@gmail.com>
2026-07-06 01:00:50 +0100

    valid palindrome

---
+++ leetcode-125-valid-palindrom.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) -1
+        while l<r:
+            while l<r and not self.alphaNum(s[l]):
+                l += 1
+            while l<r and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+        
+    def alphaNum(self, c):
+        return (ord("a") <= ord(c) <= ord("z") or
+            ord("A") <= ord(c) <= ord("Z") or
+            ord('0') <= ord(c) <= ord('9'))
